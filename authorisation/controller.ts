09855328f6e16e3c7b18a53c572a5497e6217396
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2026-08-21 15:52:10 +0200

    created the user register Auth and handled password check

--- authorisation/controller.ts
+++ authorisation/controller.ts
@@ -1,2 +1,38 @@
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import sequelize from "../common/database.ts";
+import defineUser from "../common/models/User.ts";
 
+const User = defineUser(sequelize);
+
+
+const encrypPassword = (password: string) => 
+    crypto.createHash('sha256').update(password).digest('hex');
+
+const generateAccessToken = (username: string, userId: number) => 
+    jwt.sign({ username, userId }, 'secret-key', { expiresIn: '24h' });
+
+exports.register = async (req, res) => {
+    try {
+        const { username, email, password, firstname, lastname } = req.body;
+
+        const encryptedPassword = encrypPassword(password);
+
+        const user = await User.create({
+            username,
+            email,
+            password,
+            firstname,
+            lastname,
+        });
+
+        const accessToken = generateAccessToken(username, user.id);
+        res.status(201).json({
+            success: true,
+            user: { id: user.id, username: user.username, email: user.email },
+            token: accessToken
+        });
+    } catch (err) {
+        res.status(500).json({ success: false, error: err });
+    }
+}
