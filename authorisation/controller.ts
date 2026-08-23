87d09b4e5d81fc9855907f48468db022f1c0131f
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2026-08-23 17:24:55 +0200

    added a login endpoint and integrated the encryption of the password

--- authorisation/controller.ts
+++ authorisation/controller.ts
@@ -6,17 +6,18 @@ import defineUser from "../common/models/User.ts";
 const User = defineUser(sequelize);
 
 
-const encrypPassword = (password: string) => 
+const encryptPassword = (password: string) => 
     crypto.createHash('sha256').update(password).digest('hex');
 
 const generateAccessToken = (username: string, userId: number) => 
     jwt.sign({ username, userId }, 'secret-key', { expiresIn: '24h' });
 
+// register endpoint
 const register = async ({req, res}: any) => {
     try {
         const { username, email, password, firstname, lastname } = req.body;
 
-        const encryptedPassword = encrypPassword(password);
+        const encryptedPassword = encryptPassword(password);
 
         const user = await User.create({
             username,
@@ -34,6 +35,35 @@ const register = async ({req, res}: any) => {
         });
     } catch (err) {
         res.status(500).json({ success: false, error: err });
+    }
+}
+
+// login endpoint
+const login = async ({req, res}: any) => {
+    try {
+     // retrieve the username, password
+     const { username, password } = req.body;
+
+      // encrypt the given password
+      const encrypted = encryptPassword(password);
+
+      // find the given username
+      const user = await User.find({ where : {username} });
+
+      // check if user exists and the password encrypted
+      if(!user || user.password !== encrypted) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+
+      // access the token username and id
+      const token = generateAccessToken(username, user.id);
+      res.json({ success: true, user, token });
+
+      console.log('user successfully logged in');
+
+    } catch (error) {
+      console.log('user not found', error);
+      res.status(404).json({ error: 'user not found' });
     }
 }
 
